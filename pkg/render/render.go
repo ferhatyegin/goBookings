78f90b3812abc18a45ad24f08d210577e343c43c
ferhatyegin <ferhatyegin@protonmail.com>
2023-12-15 15:48:38 +0300

    Added a simple template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,15 +3,64 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 // RenderTemplate is where program parses HTML templates and it's content to be written by the response writer
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	//Check to see if we already have the template in our cache
+	_, inMap := tc[t]
+
+	if !inMap {
+		//Need to create the template
+		log.Println("creating template and adding to cache")
+		err = CreateTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		//We have the template in the cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func CreateTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	//Parse the template
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	//Add template to the cache map
+
+	tc[t] = tmpl
+	return nil
 }
