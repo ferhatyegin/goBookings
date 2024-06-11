a7c0f0a70c0f7b217199fdd2e9b81184a8fbf24b
ferhatyegin <ferhatyegin47@gmail.com>
2024-06-11 14:35:35 +0300

    updated handlers with better error handling

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,11 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/ferhatyegin/goBookings/internal/config"
 	"github.com/ferhatyegin/goBookings/internal/forms"
+	"github.com/ferhatyegin/goBookings/internal/helpers"
 	"github.com/ferhatyegin/goBookings/internal/models"
 	"github.com/ferhatyegin/goBookings/internal/render"
 )
@@ -59,7 +59,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 		return
 	}
 
@@ -118,7 +118,8 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	out, err := json.MarshalIndent(resp, "", "     ")
 
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -148,7 +149,7 @@ func (m *Repository) ReservationsSummary(w http.ResponseWriter, r *http.Request)
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 
 	if !ok {
-		log.Println("cannot get item from session.")
+		m.App.ErrorLog.Println("Cannot get item from session")
 		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session.")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
