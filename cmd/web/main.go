e379044d490851c8ca67990eefae80a0ebd5dbbe
ferhatyegin <ferhatyegin47@gmail.com>
2024-06-11 14:22:06 +0300

    created error logger and info logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/ferhatyegin/goBookings/internal/config"
 	"github.com/ferhatyegin/goBookings/internal/handlers"
+	"github.com/ferhatyegin/goBookings/internal/helpers"
 	"github.com/ferhatyegin/goBookings/internal/models"
 	"github.com/ferhatyegin/goBookings/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // Main is the main application function
 func main() {
@@ -48,6 +52,12 @@ func run() error {
 	// Change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog 
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -67,6 +77,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
